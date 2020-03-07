2b6d476092efa8f9e5dcad0e13d0543d6fdaf818
Carlos Duque <carlosduque.hn@gmail.com>
2020-03-07 11:12:03 -0300

    added tika to extract pdf contents

--- src/indexr/index.clj
+++ src/indexr/index.clj
@@ -9,7 +9,13 @@
                              DirectoryReader]
     [org.apache.lucene.queryparser.classic QueryParser]
     [org.apache.lucene.search IndexSearcher Query ScoreDoc]
-    [org.apache.lucene.store Directory FSDirectory])
+    [org.apache.lucene.store Directory FSDirectory]
+    [org.apache.tika.exception TikaException]
+    [org.apache.tika.metadata Metadata]
+    [org.apache.tika.parser AutoDetectParser ParseContext Parser]
+    [org.apache.tika.sax BodyContentHandler]
+    [org.xml.sax ContentHandler]
+    [org.xml.sax SAXException])
   (:gen-class))
 
 (defn to-str [value]
@@ -38,16 +44,38 @@
   (with-open [reader (io/reader (:file fields))]
       (.addDocument writer (fields->doc (assoc fields :contents reader)))))
 
+(defn extract-contents [file]
+  (let [metadata (Metadata.)
+        handler  (BodyContentHandler.)
+        context  (ParseContext.)
+        parser   (AutoDetectParser.)]
+    (with-open [stream (io/input-stream file)]
+      (try
+        (.parse parser stream handler metadata context)
+        (catch TikaException e
+          (println "tika exception:" (.getMessage e)))
+        (catch Exception e
+          (println "general exception:" (.getMessage e)))))
+    (.toString handler)))
+
 ;;;;;;;
-#_(def idx-path (.toPath (io/file "/home/carlos/idx")))
+(def idx-path (.toPath (io/file "/Users/carlos/idx")))
 
-#_(def book1 {:path "/home/carlos/dev/src/indexr/resources/books/d1.txt"
-            :file (io/file "./resources/books/d1.txt")
+#_(def a-txt {:path "/home/carlos/dev/src/indexr/resources/books/txt/d1.txt"
+            :file (io/file "./resources/books/txt/d1.txt")
             :created-at (System/currentTimeMillis)})
 
-#_(def book2 {:path "/home/carlos/dev/src/indexr/resources/books/d2.txt"
-            :file (io/file "./resources/books/d2.txt")
+(def aladdin-pdf {:path "/home/carlos/dev/src/indexr/resources/books/aladdin.pdf"
+            :file (io/file "./resources/books/aladdin.pdf")
             :created-at (System/currentTimeMillis)})
 
-#_(index-file idx-path book1)
-#_(index-file idx-path book2)
+(def sicp-pdf {:path "/home/carlos/dev/src/indexr/resources/books/sicp.pdf"
+            :file (io/file "./resources/books/sicp.pdf")
+            :created-at (System/currentTimeMillis)})
+#_(def an-epub {:path "/home/carlos/dev/src/indexr/resources/books/epub/les-miserables.epub"
+            :file (io/file "./resources/books/epub/les-miserables.epub")
+            :created-at (System/currentTimeMillis)})
+
+#_(index-file idx-path a-pdf)
+
+
